fb67bd323dc72c17d3adaf2928875d6abef8d2fd
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-07-08 22:43:44 +0500

    Splash_

---
+++ app/src/main/java/com/example/fruithub/screens/Splash_Screen.kt
@@ -0,0 +1,27 @@
+package com.example.fruithub.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.fruithub.R
+
+@Composable
+fun SplashScreen()
+{
+    Column (modifier = Modifier.fillMaxSize()
+        ,horizontalAlignment = Alignment.CenterHorizontally
+        ,verticalArrangement = Arrangement.Center)
+    {
+        Image(painter = painterResource(R.drawable.logo)
+            , contentDescription = null,
+            modifier = Modifier.size(184.dp,206.dp)
+        )
+    }
+}
---
+++ app/src/main/java/com/example/fruithub/screens/Wellcome_Screen.kt
@@ -0,0 +1,98 @@
+package com.example.fruithub.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.fruithub.R
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun Wellcome_Screen()
+{
+   Column (modifier = Modifier.fillMaxSize().background(color = Color.White))
+   {
+       Box(modifier = Modifier.fillMaxWidth()
+           .weight(0.065f)
+           .background(brush = Brush.verticalGradient(colors =
+               listOf(Color(0xFFFF8C42),Color(0xFFFF7A2B))))
+           ,contentAlignment = Alignment.Center)
+       {
+           Box(
+               modifier = Modifier.size(280.dp)
+                   .background(color = Color.White.copy(alpha = 0.1f),shape = CircleShape)
+               ,contentAlignment = Alignment.Center
+           ) {
+               Image(
+                   painter = painterResource(R.drawable.fruit_basket_image),
+                   contentDescription = "",
+                   modifier = Modifier.size(184.dp, 206.dp)
+               )
+
+           }
+       }
+       Column (modifier = Modifier.fillMaxWidth().weight(0.035f).background(color = Color.White),
+           horizontalAlignment = Alignment.CenterHorizontally,
+           verticalArrangement = Arrangement.Center
+       )
+       {
+           Column (
+                horizontalAlignment = Alignment.CenterHorizontally
+           ){
+           Text(
+               text = "Get The Freshest Salad Combo",
+               color = Color.Black,
+               fontSize = 22.sp,
+               fontWeight = FontWeight.Bold,
+               textAlign = TextAlign.Center
+           )
+               Spacer(modifier = Modifier.height(26.dp))
+
+           Text(
+               text="We Deliver the best Freshest Salad in town order for a combo today !!!",
+               color = Color.Black,
+               fontSize = 18.sp,
+               textAlign = TextAlign.Center
+           )
+               Spacer(modifier = Modifier.height(26.dp))
+
+               Button(onClick = {
+
+
+
+               }, modifier = Modifier.fillMaxWidth()
+                   .height(56.dp),
+                   colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF8C42)),
+                   shape = RoundedCornerShape(16.dp))
+
+               { Text("Lets Continue",
+                   fontSize = 16.sp,
+                   fontWeight = FontWeight.Medium,
+                   color = Color.White) }
+           }
+           }
+       }
+   }
